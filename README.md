67fb12a34b752d71132ab242d5cc5ce7c62c0952
svidoso <svidoso@svidoso.com>
2018-05-09 10:51:15 +0200

    hot redeploy; intellij start server + redeploy

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ## Example maven project for tomcat embedded and java server faces
 
-### build & run
+### Build & Run
     mvn tomcat7:run
+    
+### Links
+    
++ [tomcat7 plugin documentation](http://tomcat.apache.org/maven-plugin-2.2/tomcat7-maven-plugin/plugin-info.html)
--- src/main/java/mygroup/UserBean.java
+++ src/main/java/mygroup/UserBean.java
@@ -4,12 +4,13 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
 
-@Named
+@ManagedBean
 @SessionScoped
 public class UserBean implements Serializable {
     
